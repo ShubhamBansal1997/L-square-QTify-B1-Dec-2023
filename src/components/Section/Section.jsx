2bd61776c6df8ecd674926c04726e4cea91a84b5
Shubham Bansal <shubhambansal17@hotmail.com>
2023-12-25 21:20:22 +0530

    hide the show all button

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -34,7 +34,7 @@ export default function Section({ title, data, filterSource, type}) {
         <div>
             <div className={styles.header}>
                 <h3>{title}</h3>
-                <h4 className={styles.toggleText} onClick={handleToggle}>{!carouselToggle ? "Collapse All": "Show All"}</h4>
+                {!showFilters && (<h4 className={styles.toggleText} onClick={handleToggle}>{!carouselToggle ? "Collapse All": "Show All"}</h4>)}
             </div>
             {showFilters && (
                 <div className={styles.filterWrapper}>
